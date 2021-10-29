f2eb6b15d90abdb24679c33397741915c33699f9
Alan Fidelino <55466923+afimonsido@users.noreply.github.com>
2021-10-29 22:47:47 +1100

    Add blogs component

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs,setBlogs] = useState([
@@ -9,15 +10,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            <h1>Blog List</h1>
-            {
-                blogs.map((blog)=>(
-                    <div className="blog-preview" key={blog.id}>
-                        <h2>{blog.title}</h2>
-                        <p className="author">{blog.author}</p>
-                    </div>
-                ))
-            }
+            <BlogList title="All Blogs" blogs={blogs}/>
         </div>
     );
 }
