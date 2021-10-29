b231bc8208e0c708852be81245eae2b342b48e81
Alan Fidelino <55466923+afimonsido@users.noreply.github.com>
2021-10-29 22:22:05 +1100

    Test React Dev Tools

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [name,setName]= useState('Alan');
+    const [age,setAge] = useState(50);
+
+    const handleClick = ()=>{
+        setName("Carlos");
+        setAge(27)
+    }
     return (
         <div className="home">
             <h1>Homepage</h1>
+            <p>{name} is {age} years old.</p>
+            <button onClick={handleClick}>Change the name</button>
         </div>
     );
 }
