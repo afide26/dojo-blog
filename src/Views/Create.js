c291191676a403401ba2b13719a486481738e283
Alan Fidelino <55466923+afimonsido@users.noreply.github.com>
2021-10-30 18:47:25 +1100

    Handlingf form submission

--- src/Views/Create.js
+++ src/Views/Create.js
@@ -7,34 +7,37 @@ const Create = () => {
     return (
         <div className="create">
             <h2>Add a new entry</h2>
-            <label htmlFor="title">Title:</label>
-            <input 
-                type="text" 
-                id="title" 
-                placeholder="Add a title to your entry"
-                required
-                onChange = {(e)=>setTitle(e.target.value)}
-                />
-            <label htmlFor="body">Text body:</label>
-            <textarea 
-                name="body" 
-                id="body"
-                required
-                onChange={e=>setBody(e.target.value)}
-                />
-            <label htmlFor="select-author">Select the author name:</label>
-            <select 
-                name="select-author" 
-                id="select-author"
-                value={author}
-                onChange={(e)=> setAuthor(e.target.value)}>
-                <option value="mario">Mario</option>
-                <option value="yoshi">Yoshi</option>
-            </select>
-            <button>Add entry</button>
-            <p>{title}</p>
-            <p>{body}</p>
-            <p className="author">{author}</p>
+            <form onSubmit={(e)=>{
+                e.preventDefault();
+                const blog = {title, author, body}
+                console.log(blog)
+            }}>
+                <label htmlFor="title">Title:</label>
+                <input 
+                    type="text" 
+                    id="title" 
+                    placeholder="Add a title to your entry"
+                    required
+                    onChange = {(e)=>setTitle(e.target.value)}
+                    />
+                <label htmlFor="body">Text body:</label>
+                <textarea 
+                    name="body" 
+                    id="body"
+                    required
+                    onChange={e=>setBody(e.target.value)}
+                    />
+                <label htmlFor="select-author">Select the author name:</label>
+                <select 
+                    name="select-author" 
+                    id="select-author"
+                    value={author}
+                    onChange={(e)=> setAuthor(e.target.value)}>
+                    <option value="mario">Mario</option>
+                    <option value="yoshi">Yoshi</option>
+                </select>
+                <button>Add entry</button>
+            </form>
         </div>
     );
 }
