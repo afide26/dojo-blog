805f86053ef1bb04eb68fb57e62ed8efa9995906
Alan Fidelino <55466923+afimonsido@users.noreply.github.com>
2021-10-30 18:39:21 +1100

    Creating forms page

--- src/Views/Create.js
+++ src/Views/Create.js
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('')
     return (
         <div className="create">
-            <h2>Create a New Blog</h2>
+            <h2>Add a new entry</h2>
+            <label htmlFor="title">Title:</label>
+            <input 
+                type="text" 
+                id="title" 
+                placeholder="Add a title to your entry"
+                required
+                onChange = {(e)=>setTitle(e.target.value)}
+                />
+            <label htmlFor="body">Text body:</label>
+            <textarea 
+                name="body" 
+                id="body"
+                required
+                onChange={e=>setBody(e.target.value)}
+                />
+            <label htmlFor="select-author">Select the author name:</label>
+            <select 
+                name="select-author" 
+                id="select-author"
+                value={author}
+                onChange={(e)=> setAuthor(e.target.value)}>
+                <option value="mario">Mario</option>
+                <option value="yoshi">Yoshi</option>
+            </select>
+            <button>Add entry</button>
+            <p>{title}</p>
+            <p>{body}</p>
+            <p className="author">{author}</p>
         </div>
     );
 }
