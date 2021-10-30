29c9eeffdaa5327cfaa28f45198b85a752e2682e
Alan Fidelino <55466923+afimonsido@users.noreply.github.com>
2021-10-30 14:00:12 +1100

    Add json data using json server

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -2,28 +2,20 @@ import { useState,useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs,setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ])
+    const [blogs,setBlogs] = useState(null)
 
-    const [name,setName] = useState('Alan');
-    const handleDelete = (id)=>{
-        const newBlogs = blogs.filter((blog)=> blog.id !== id);
-
-        setBlogs(newBlogs);
-    }
 
     useEffect(()=>{
-        console.log('use effect ran');
-        console.log(name);
-    },[name])
+        fetch('http://localhost:8000/blogs')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            setBlogs(data);
+        })
+    },[])
     return (
         <div className="home">
-            <BlogList title="All Blogs" blogs={blogs} handleDelete={handleDelete}/>
-            <button onClick={()=>setName("Carlos")}>Change the name</button>
-            <p>{name}</p>
+            {blogs && <BlogList title="All Blogs" blogs={blogs} />}
         </div>
     );
 }
